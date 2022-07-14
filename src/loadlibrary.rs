6c1e2057fbcee641c4b023787be4d7e44257ccb3
Koen Molenaar <koen.molenaar26@gmail.com>
2022-07-14 23:30:10 +0200

    First refactor

--- src/loadlibrary.rs
+++ src/loadlibrary.rs
@@ -1,8 +1,38 @@
-use std::ffi::c_void;
+use std::mem::transmute_copy;
+use std::os::raw::c_char;
+use std::{
+    ffi::{c_void, CString},
+    ptr::NonNull,
+};
 
-type HModule = *const c_void;
+// Types
 
+type HModule = NonNull<*const c_void>;
+type FarProc = NonNull<*const c_void>;
+
+// Structs
+
+#[derive(Debug)]
 pub struct Library {
-  handle: HModule,
+    handle: HModule,
 }
 
+// Implementations
+extern "stdcall" {
+    fn LoadLibraryA(name: *const c_char) -> Option<HModule>;
+    fn GetProcAddress(module: HModule, name: *const c_char) -> Option<FarProc>;
+}
+
+impl Library {
+    pub fn new(name: &str) -> Option<Self> {
+        let name = CString::new(name).expect("Invalid library name");
+        let res = unsafe { LoadLibraryA(name.as_ptr()) };
+        res.map(|module| Library { handle: module })
+    }
+
+    pub fn get_proc<T>(&self, name: &str) -> Option<T> {
+        let name = CString::new(name).expect("Invalid proc name");
+        let res = unsafe { GetProcAddress(self.handle, name.as_ptr()) };
+        res.map(|proc| unsafe { transmute_copy(&proc) })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 mod loadlibrary;
 
 use core::fmt;
-use std::{ffi::c_void, mem::{transmute, self}};
+use std::{
+    ffi::c_void,
+    mem::{self},
+};
 
+use loadlibrary::Library;
 use pretty_hex::PrettyHex;
 
 struct IpAddr([u8; 4]);
 
 impl fmt::Debug for IpAddr {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-      let [a, b, c, d] = self.0;
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let [a, b, c, d] = self.0;
 
-      write!(f, "{}.{}.{}.{}", a, b, c, d)
-  }
+        write!(f, "{}.{}.{}.{}", a, b, c, d)
+    }
 }
 
-
-
 fn main() {
-  unsafe {
-    let h = LoadLibraryA("IPHLPAPI.dll\0".as_ptr());
-    let IcmpCreateFile: IcmpCreateFile = transmute(GetProcAddress(h, "IcmpCreateFile\0".as_ptr()));
-    let IcmpSendEcho: IcmpSendEcho = transmute(GetProcAddress(h, "IcmpSendEcho\0".as_ptr()));
+    let iphlp = Library::new("IPHLPAPI.dll").unwrap();
+    let IcmpSendEcho: IcmpSendEcho = iphlp.get_proc("IcmpSendEcho").unwrap();
+    let IcmpCreateFile: IcmpCreateFile = iphlp.get_proc("IcmpCreateFile").expect("error");
 
     let handle: Handle = IcmpCreateFile();
 
@@ -34,76 +35,75 @@ fn main() {
 
     let mut reply_buf = vec![0u8; reply_buf_size];
 
-    let ip_options: IpOptionInformation = IpOptionInformation { ttl: 128, tos: 0, flags: 0, options_size: 0, options_data: 0 };
-  
-    let ret = IcmpSendEcho(
-      handle,
-      IpAddr([8,8,8,8]),
-      data.as_ptr(),
-      data.len() as u16,
-      Some(&ip_options),
-      reply_buf.as_mut_ptr(),
-      reply_buf_size as u32,
-      5000,
-    );
+    let ip_options: IpOptionInformation = IpOptionInformation {
+        ttl: 128,
+        tos: 0,
+        flags: 0,
+        options_size: 0,
+        options_data: 0,
+    };
+
+    let ret = unsafe {
+        IcmpSendEcho(
+            handle,
+            IpAddr([8, 8, 8, 8]),
+            data.as_ptr(),
+            data.len() as u16,
+            Some(&ip_options),
+            reply_buf.as_mut_ptr(),
+            reply_buf_size as u32,
+            5000,
+        )
+    };
 
     if (ret == 0) {
-      panic!("IcmpSendEcho failed! ret = {}", ret)
+        panic!("IcmpSendEcho failed! ret = {}", ret)
     }
 
-    let reply: &IcmpEchoReply = mem::transmute(&reply_buf[0]);
+    unsafe {
+        let reply: &IcmpEchoReply = mem::transmute(&reply_buf[0]);
 
-    let reply_data: *const u8 = mem::transmute(&reply_buf[reply_size + 8]);
+        let reply_data: *const u8 = mem::transmute(&reply_buf[reply_size + 8]);
 
-    let reply_data = std::slice::from_raw_parts(reply_data, reply.data_size as usize);
+        let reply_data = std::slice::from_raw_parts(reply_data, reply.data_size as usize);
 
-    println!("{:#?}", reply_data.hex_dump())
-  }
+        println!("{:#?}", reply_data.hex_dump())
+    };
 }
 
-type HModule = *const c_void;
-type FarProc = *const c_void;
 type Handle = *const c_void;
+
 type IcmpSendEcho = extern "stdcall" fn(
-  handle: Handle,
-  dest: IpAddr,
-  request_data: *const u8,
-  request_size: u16,
-  request_options: Option<&IpOptionInformation>,
-  reply_buffer: *mut u8,
-  reply_size: u32,
-  timeout: u32,
+    handle: Handle,
+    dest: IpAddr,
+    request_data: *const u8,
+    request_size: u16,
+    request_options: Option<&IpOptionInformation>,
+    reply_buffer: *mut u8,
+    reply_size: u32,
+    timeout: u32,
 ) -> u32;
+
 type IcmpCreateFile = extern "stdcall" fn() -> Handle;
 
 #[repr(C)]
 #[derive(Debug)]
 struct IpOptionInformation {
-  ttl: u8,
-  tos: u8,
-  flags: u8,
-  options_size: u8,
-  options_data: u32,
+    ttl: u8,
+    tos: u8,
+    flags: u8,
+    options_size: u8,
+    options_data: u32,
 }
 
 #[repr(C)]
 #[derive(Debug)]
 struct IcmpEchoReply {
-  address: IpAddr,
-  status: u32,
-  rtt: u32,
-  data_size: u16,
-  reserved: u16,
-  data: *const u8,
-  options: IpOptionInformation,
+    address: IpAddr,
+    status: u32,
+    rtt: u32,
+    data_size: u16,
+    reserved: u16,
+    data: *const u8,
+    options: IpOptionInformation,
 }
-
-extern "stdcall" {
-  fn LoadLibraryA(name: *const u8) -> HModule;
-  fn GetProcAddress(module: HModule, name: *const u8) -> FarProc;
-  fn GetLastError() -> u16;
-}
-
-
-
- 

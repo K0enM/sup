b03385ae2674ab35d2825f3caf03af3a89de2200
Koen Molenaar <koen.molenaar26@gmail.com>
2022-07-15 16:59:17 +0200

    Article 5

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::{env, process::exit};
+use std::{env, error::Error, process::exit};
 
 pub mod icmp;
 pub mod ipv4;
 mod loadlibrary;
 
-fn main() {
-  let arg = env::args().nth(1).unwrap_or_else(|| {
-    println!("Usage: sup DEST");
-    exit(1)
-  });
+fn main() -> Result<(), Box<dyn Error>>{
+    let arg = env::args().nth(1).unwrap_or_else(|| {
+        println!("Usage: sup DEST");
+        exit(1)
+    });
 
-  let dest = ipv4::Addr::parse(arg).unwrap();
-  icmp::ping(ipv4::Addr([0, 0, 0, 0])).expect("Ping failed");
+    icmp::ping(arg.parse()?)?;
+
+    Ok(())
 }
